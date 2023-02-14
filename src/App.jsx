d10c0218da4fe535b2d6289ecdd1ab00c9a0615a
Ender Çelik <64286011+EnderCelik0@users.noreply.github.com>
2023-02-15 01:42:24 +0300

    x

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import ProfileCard from './components/ProfileCard';
 export default function App() {
   {
     return (
-      <div className='flex flex-col items-center justify-center w-screen'>
+      <div
+        className='flex flex-col items-center justify-center w-screen'
+      >
         <ProfileCard
           profilePicture='../src/assets/images/image-jeremy.png'
           name='Jeremy Robson'
